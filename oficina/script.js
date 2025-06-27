733447d54f271a048a4ae497ca10cdd1ed89adbc
JCM2 <joao.moreno.monteiro@escola.pr.gov.br>
2025-06-27 13:43:05 -0300

    e

--- oficina/script.js
+++ oficina/script.js
@@ -1,8 +1,7 @@
 const botao = document.getElementById("botao");
 const corpo = document.body;
 
-corpo.addEventListener(
-    "mousemove", function (evento) {
+corpo.addEventListener("mousemove", function (evento) {
         let mouseX = evento.clientX;
         let mouseY = evento.clientY;
         console.log(mouseX,mouseY);
